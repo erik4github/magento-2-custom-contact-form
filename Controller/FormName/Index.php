0d9695a0772833189846b31a5d9f3de65edef88a
erik4github <erik4github@gmail.com>
2018-10-17 17:30:39 -0500

    Create Index.php

---
+++ Controller/FormName/Index.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace VendorName\ContactForms\Controller\FormName;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor for Form Name
+     * Renders page
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
